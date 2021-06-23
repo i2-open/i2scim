19a777e5262543916298659dfd7f0cc8857812ad
Phil Hunt <independentidentity@gmail.com>
2021-06-23 15:10:26 -0700

    [SCIM-71] Changed copyright to Apache License 2.0

--- i2scim-core/src/main/java/com/independentid/scim/security/AccessControl.java
+++ i2scim-core/src/main/java/com/independentid/scim/security/AccessControl.java
@@ -1,16 +1,17 @@
 /*
- * Copyright (c) 2021.
+ * Copyright 2021.  Independent Identity Incorporated
  *
- * Confidential and Proprietary
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This unpublished source code may not be distributed outside
- * “Independent Identity Org”. without express written permission of
- * Phillip Hunt.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * People at companies that have signed necessary non-disclosure
- * agreements may only distribute to others in the company that are
- * bound by the same confidentiality agreement and distribution is
- * subject to the terms of such agreement.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.independentid.scim.security;
